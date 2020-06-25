0ad5ee6ca2018f974d227b3454713ed66f0ba5bf
indyminhas <66227608+indyminhas@users.noreply.github.com>
2020-06-24 21:56:48 -0700

    created readme, updated html, css, and script

--- script.js
+++ script.js
@@ -1,51 +1,111 @@
-/*SEARCH BY USING A CITY NAME (e.g. athens) OR A COMMA-SEPARATED CITY NAME ALONG WITH THE COUNTRY CODE (e.g. athens,gr)*/
-const form = document.querySelector(".top-banner form");
-const input = document.querySelector(".top-banner input");
-const msg = document.querySelector(".top-banner .msg");
-const list = document.querySelector(".ajax-section .cities");
-/*PUT YOUR OWN KEY HERE - THIS MIGHT NOT WORK
-SUBSCRIBE HERE: https://home.openweathermap.org/users/sign_up*/
-const apiKey = "465a97c2ba8fa356aa139c5a5ea680d0";
+$(".current-box").hide();
+$(".forecast-banner").hide();
+var forecastdisplay;
 
-form.addEventListener("submit", e => {
-  e.preventDefault();
-  const listItems = list.querySelectorAll(".ajax-section .city");
-  const inputVal = input.value;
+//Pulls previous city searches from local storage.
+function allStorage() {
+    var values = [],
+        keys = Object.keys(localStorage),
+        i = keys.length;
+    while ( i-- ) {
+        values.push( localStorage.getItem(keys[i]));
+    }
+    for (j = 0; j < values.length; j++) {
+        $(".prev-list").prepend("<button class='prev-city mt-1'>" + values[j] + "</button>");
+    }
+}
+allStorage();
 
-  //ajax here
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
-
-  fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      const { main, name, sys, weather } = data;
-      const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${
-        weather[0]["icon"]
-      }.svg`;
-
-      const li = document.createElement("li");
-      li.classList.add("city");
-      const markup = `
-        <h2 class="city-name" data-name="${name},${sys.country}">
-          <span>${name}</span>
-          <sup>${sys.country}</sup>
-        </h2>
-        <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup></div>
-        <figure>
-          <img class="city-icon" src="${icon}" alt="${
-        weather[0]["description"]
-      }">
-          <figcaption>${weather[0]["description"]}</figcaption>
-        </figure>
-      `;
-      li.innerHTML = markup;
-      list.appendChild(li);
-    })
-    .catch(() => {
-      msg.textContent = "Please search for a valid city!!!";
-    });
-
-  msg.textContent = "";
-  form.reset();
-  input.focus();
+//Clears all local storage items and previous searches from the page.
+$(".clear").on("click", function() {
+    localStorage.clear();
+    $(".prev-city").remove();
 });
+
+//This function collects all the info from the weather APIs to display on the page
+$(".search").on("click", function() {
+    var subject = $(".subject").val();
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + subject + "&appid=465a97c2ba8fa356aa139c5a5ea680d0";
+    var queryURL2 = "https://api.openweathermap.org/data/2.5/forecast?q=" + subject + "&appid=465a97c2ba8fa356aa139c5a5ea680d0";
+    var lat;
+    var lon;
+    if (forecastdisplay === true) {
+        $(".forecast-day").remove();
+        forecastdisplay = false;
+    }
+
+//This first ajax request collects current weather data and converts info into what we want to display.
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+        statusCode: {
+            404: function() {
+              return;
+            }
+          }    
+    }).then(function(response){
+        console.log(response);
+        $(".prev-list").prepend("<button class='prev-city mt-1'>" + subject + "</button>");
+        localStorage.setItem(subject, subject);
+        $(".current-box").show();
+        $(".forecast-banner").show();
+        var iconcode = response.weather[0].icon;
+        var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+        $(".icon").attr('src', iconurl)
+        lat = response.coord.lat;
+        lon = response.coord.lon;
+        $(".current-city").text(response.name + " " + moment().format('l'));
+        var currentTemp = response.main.temp * (9/5) - 459.67;
+        $(".current-temp").text("Temperature: " + currentTemp.toFixed(1) + " °F");
+        $(".current-hum").text("Humidity: " + response.main.humidity + "%");
+        $(".current-wind").text("Wind Speed: " + response.wind.speed + " MPH");
+        queryURL = "http://api.openweathermap.org/data/2.5/uvi/forecast?&appid=3c34658c8e0e9fdb71064b81293a3704&lat=" + lat + "&lon=" + lon;
+//This is nested ajax request that gets the UV index but uses longitude and latitude from the previous ajax request to do so.
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response){
+            $(".current-uv").text("UV Index: " + response[0].value);
+        })
+    })
+
+//This ajax request collects weather data for the next 5 days (specifically it is grabbing the stays from noon, as opposed to every few hours)
+    $.ajax({
+        url: queryURL2,
+        method: "GET"
+    }).then(function(response){
+        var forecastTimes = response.list;
+        for (i = 0; i < forecastTimes.length; i++) {
+            if (forecastTimes[i].dt_txt[12] === "2") {
+                var forecastdate = forecastTimes[i].dt_txt;
+                var forecastdatedisplay = forecastdate.charAt(5) + forecastdate.charAt(6) + "/" + forecastdate.charAt(8) + forecastdate.charAt(9) +
+                "/" + forecastdate.charAt(0) + forecastdate.charAt(1) + forecastdate.charAt(2) + forecastdate.charAt(3);
+                var forecasticon = forecastTimes[i].weather[0].icon;
+                var forecasticonurl = "http://openweathermap.org/img/w/" + forecasticon + ".png";
+                var forecastTemp = forecastTimes[i].main.temp * (9/5) - 459.67;
+                var forecastHum = forecastTimes[i].main.humidity;
+                if (forecastdisplay === false || forecastdisplay === undefined) {
+                    $(".forecast-list").append("<div class='my-3 pb-3 col-md-2 col-lg-2 forecast-day'>" +
+                    "<h5>" + forecastdatedisplay + "<h5>" +
+                    "<img class='ficon' src=" + forecasticonurl + " alt='Weather icon'>" + 
+                    "<div>Temp: " + forecastTemp.toFixed(1) + " °F" + 
+                    "</div><div>Humidity: " + forecastHum + 
+                    "%</div></div></div>");
+                } 
+            }
+        }
+        forecastdisplay = true;
+    })
+});
+
+//This will search the weather stats for the previous city when said city is clicked on.
+$(document).on("click", ".prev-city", function() {
+    var subject = $(this).text();
+    $(".subject").val(subject);
+    $(".search").click();
+    $(this).remove();
+});
+
+
+
+       
